ff2c47c0a23dfdbfc32d63e4d4b4a527964a2ff0
Vijendra <pvijendra756@gmail.com>
2026-02-26 12:34:10 +0530

    Removed some erros

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-const app = require("./app");
-const PORT = process.env.PORT || 3000;
+import app from "./app";
+const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running in ${PORT}`);
 });
